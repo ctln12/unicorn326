64c4f1bc0029431c5212dc5dbd1ccd2d0d11ced8
Cataline Baert <snuf_kin@yahoo.com>
2019-12-30 13:59:19 +0100

    add date of birth as permitted parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,9 +10,9 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :country, :date_of_birth, :phone_number, :photo_url])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :country, :date_of_birth, :phone_number, :photo_url])
   end
-  
+
   def configure_permitted_parameters_tutor
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name,:phone_number, :photo_url, :subjects, :spoken_languages, :currency, :price])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :date_of_birth, :phone_number, :photo_url, :subjects, :spoken_languages, :currency, :price])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name,:phone_number, :photo_url, :subjects, :spoken_languages, :currency, :price])
   end
 end
