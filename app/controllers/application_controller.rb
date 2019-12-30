e51edb9ca0f4578b06c3c9064acc2a49ac762665
Cataline Baert <snuf_kin@yahoo.com>
2019-12-30 10:44:25 +0100

    add phone number to permitted parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :photo_url, :subjects, :spoken_languages, :currency, :price])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name,:phone_number, :photo_url, :subjects, :spoken_languages, :currency, :price])
   end
 end
