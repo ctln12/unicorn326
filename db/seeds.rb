6c3f65ce6b05566ebf936b0eb1d84049c500fd10
Cataline Baert <snuf_kin@yahoo.com>
2020-01-07 18:36:01 +0100

    change Subjects for Subject in the part creating the subjects and change Subjects for Language in the part creating the languages

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,44 +8,44 @@
 
 puts 'Creating subjects...'
 
-english = Subjects.new(name: "English")
+english = Subject.new(name: "English")
 english.save!
 
-mathematics = Subjects.new(name: "Mathematics")
+mathematics = Subject.new(name: "Mathematics")
 mathematics.save!
 
-physics = Subjects.new(name: "Physics")
+physics = Subject.new(name: "Physics")
 physics.save!
 
-biology = Subjects.new(name: "Biology")
+biology = Subject.new(name: "Biology")
 biology.save!
 
-finance = Subjects.new(name: "Finance")
+finance = Subject.new(name: "Finance")
 finance.save!
 
-accounting = Subjects.new(name: "Accounting")
+accounting = Subject.new(name: "Accounting")
 accounting.save!
 
 puts 'Finished!'
 
 puts 'Creating Languages...'
 
-english = Subjects.new(name: "English")
+english = Language.new(name: "English")
 english.save!
 
-italian = Subjects.new(name: "Italian")
+italian = Language.new(name: "Italian")
 italian.save!
 
-french = Subjects.new(name: "French")
+french = Language.new(name: "French")
 french.save!
 
-german = Subjects.new(name: "German")
+german = Language.new(name: "German")
 german.save!
 
-spanish = Subjects.new(name: "Spanish")
+spanish = Language.new(name: "Spanish")
 spanish.save!
 
-danish = Subjects.new(name: "Danish")
+danish = Language.new(name: "Danish")
 danish.save!
 
 puts 'Finished!'
