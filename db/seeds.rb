b4e60eb29e2fdf2bfb17f208b84e482b543ef8b6
Cataline Baert <snuf_kin@yahoo.com>
2020-01-08 15:54:12 +0100

    destroy all subjects and languages before seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Destroying subjects...'
+Subject.destroy_all
+puts 'Destroying languages...'
+Language.destroy_all
 
 puts 'Creating subjects...'
 
