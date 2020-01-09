07553a168344ea996336beaff3e62fe5f7ec22b9
Cataline Baert <snuf_kin@yahoo.com>
2020-01-09 15:06:20 +0100

    remove column name from spoken languages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_09_140014) do
+ActiveRecord::Schema.define(version: 2020_01_09_140428) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,6 @@ ActiveRecord::Schema.define(version: 2020_01_09_140014) do
   end
 
   create_table "spoken_languages", force: :cascade do |t|
-    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
