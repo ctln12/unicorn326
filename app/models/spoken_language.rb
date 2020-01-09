54973d46b3007d9693819f7872d6e20343b40c04
Cataline Baert <snuf_kin@yahoo.com>
2020-01-09 15:11:49 +0100

    add associations

--- app/models/spoken_language.rb
+++ app/models/spoken_language.rb
@@ -1,2 +1,4 @@
 class SpokenLanguage < ApplicationRecord
+  belongs_to :tutor
+  belongs_to :language
 end
--- app/models/tutor.rb
+++ app/models/tutor.rb
@@ -2,6 +2,7 @@ class Tutor < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :taught_lessons
+  has_many :spoken_languages
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
