1713491d9d500a5bc0680c617b8428560578e3c0
Cataline Baert <snuf_kin@yahoo.com>
2020-01-09 14:32:54 +0100

    delete foreign key tutor to subjects

--- db/migrate/20200109132106_add_tutor_to_subjects.rb
+++
@@ -1,5 +0,0 @@
-class AddTutorToSubjects < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :subjects, :tutor, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_09_132106) do
+ActiveRecord::Schema.define(version: 2020_01_09_123608) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,8 +43,6 @@ ActiveRecord::Schema.define(version: 2020_01_09_132106) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "tutor_id"
-    t.index ["tutor_id"], name: "index_subjects_on_tutor_id"
   end
 
   create_table "tutors", force: :cascade do |t|
@@ -76,5 +74,4 @@ ActiveRecord::Schema.define(version: 2020_01_09_132106) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "subjects", "tutors"
 end
