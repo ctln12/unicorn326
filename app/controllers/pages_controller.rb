df828da3fade10da0a91d29d3a0c01ad94b160c9
Cataline Baert <snuf_kin@yahoo.com>
2020-01-10 16:23:18 +0100

    remove skip before action

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,4 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_student!, only: [:home]
-  skip_before_action :authenticate_tutor!, only: [:home]
-
   def home
   end
 end
