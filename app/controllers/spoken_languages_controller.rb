3ca99bcd18f9b380a239dca56d92e0c896984c05
Cataline Baert <snuf_kin@yahoo.com>
2020-01-10 17:59:15 +0100

    implement methods new and create

--- app/controllers/spoken_languages_controller.rb
+++ app/controllers/spoken_languages_controller.rb
@@ -1,7 +1,14 @@
 class SpokenLanguagesController < ApplicationController
   def new
+    @spoken_language = SpokenLanguage.new
   end
 
   def create
+    @spoken_language = SpokenLanguage.new(language_id: params[:spoken_language][:language_id].to_i, tutor_id: params[:spoken_language][:tutor_id].to_i)
+    @spoken_language.save
+
+    if @spoken_language.save
+      redirect_to tutor_path(current_tutor.id)
+    end
   end
 end
