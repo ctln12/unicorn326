fbcb01c8635c365c9f02b99d60d50145bc41f15f
Cataline Baert <snuf_kin@yahoo.com>
2020-01-10 16:42:52 +0100

    refactor routes for subjects and languages using resouces

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   devise_for :tutors, paths: 'tutors', controllers: { sessions: 'tutors/sessions' }
 
   root to: 'pages#home'
-  get 'subjects/index', to: 'subjects#index'
-  get 'languages/index', to: 'languages#index'
+  resources :subjects, :only => [:index]
+  resources :languages, :only => [:index]
   resources :tutors, :only => [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
