44fa18d0528619b1aba592b4f94f9215456a5fae
Cataline Baert <snuf_kin@yahoo.com>
2020-01-10 11:47:54 +0100

    generate controller languages and add skip before action authentication for student and tutor only for index

---
+++ app/controllers/languages_controller.rb
@@ -0,0 +1,6 @@
+class LanguagesController < ApplicationController
+  skip_before_action :authenticate_student!, only: :index
+  skip_before_action :authenticate_tutor!, only: :index
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
   get 'subjects/index', to: 'subjects#index'
+  get 'languages/index', to: 'languages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
