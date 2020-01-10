939f4770fd00b48a07b6d168bbcdd856c91a63eb
Cataline Baert <snuf_kin@yahoo.com>
2020-01-10 16:46:07 +0100

    add students show path

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   resources :subjects, :only => [:index]
   resources :languages, :only => [:index]
   resources :tutors, :only => [:show]
+  resources :students, :only => [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
