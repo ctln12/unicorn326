8bcbb006639f24a79e1b5c5ea2fd0e537050614e
Cataline Baert <snuf_kin@yahoo.com>
2020-01-13 10:57:01 +0100

    modify create method and add index, edit, update, destroy methods

--- app/controllers/spoken_languages_controller.rb
+++ app/controllers/spoken_languages_controller.rb
@@ -1,15 +1,39 @@
 class SpokenLanguagesController < ApplicationController
+  def index
+    @spoken_languages = SpokenLanguage.all
+  end
+
   def new
     @spoken_language = SpokenLanguage.new
   end
 
   def create
-    @tutor = Tutor.find(current_tutor.id)
     params[:language_id].each do |language|
-      @spoken_language = SpokenLanguage.new(language_id: language, tutor_id: @tutor.id)
+      @spoken_language = SpokenLanguage.new(language_id: language, tutor_id: current_tutor.id)
       @spoken_language.save
     end
+    redirect_to tutor_path(current_tutor)
+  end
 
-    redirect_to tutor_path(current_tutor.id) if @spoken_language.save
+  def edit
+    @spoken_language = SpokenLanguage.find(params[:id])
+  end
+
+  def update
+    @spoken_language = SpokenLanguage.find(params[:id])
+    @spoken_language.update(spoken_language_params)
+    redirect_to tutor_path(current_tutor)
+  end
+
+  def destroy
+    @spoken_language = SpokenLanguage.find(params[:id])
+    @spoken_language.destroy
+    redirect_to tutor_path(current_tutor)
+  end
+
+  private
+
+  def spoken_language_params
+    params.require(:spoken_language).permit(:language_id)
   end
 end
