265501415913e4a0b16d4327ed0287135bf6b4d8
alessandro buccarelli <abuccarelli@hotmail.com>
2020-01-13 11:19:22 +0100

    added spoken languages and subjects linked with tutors in the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -37,6 +37,18 @@ finance.save
 accounting = Subject.new(name: "Accounting")
 accounting.save
 
+chemistry = Subject.new(name: "Chemistry")
+chemistry.save
+
+french = Subject.new(name: "French")
+french.save
+
+philosophy = Subject.new(name: "Phylosophy")
+philosophy.save
+
+statistics = Subject.new(name: "Statistics")
+statistics.save
+
 puts 'Finished!'
 puts '-----------------------------'
 
@@ -82,5 +94,55 @@ john = Tutor.new(first_name: 'John', last_name: 'Kendall', date_of_birth: '1972-
 john.save
 georges = Tutor.new(first_name: 'Georges', last_name: 'Till', date_of_birth: '1980-02-03', country: 'CH', phone_number: '41786272034', currency: 'CHF', price: 30, email: 'george.till@tutor.com', password: '123456')
 georges.save
+puts 'Finished!'
+puts '-----------------------------'
+
+puts 'Creating Taught Lessons...'
+taughtlesson1 = TaughtLesson.new(subject_id: 2, tutor_id: 1)
+taughtlesson1.save
+taughtlesson2 = TaughtLesson.new(subject_id: 3, tutor_id: 1)
+taughtlesson2.save
+taughtlesson3 = TaughtLesson.new(subject_id: 7, tutor_id: 1)
+taughtlesson3.save
+taughtlesson4 = TaughtLesson.new(subject_id: 2, tutor_id: 2)
+taughtlesson4.save
+taughtlesson5 = TaughtLesson.new(subject_id: 4, tutor_id: 2)
+taughtlesson5.save
+taughtlesson6 = TaughtLesson.new(subject_id: 2, tutor_id: 3)
+taughtlesson6.save
+taughtlesson7 = TaughtLesson.new(subject_id: 8, tutor_id: 3)
+taughtlesson7.save
+taughtlesson8 = TaughtLesson.new(subject_id: 9, tutor_id: 3)
+taughtlesson8.save
+taughtlesson9 = TaughtLesson.new(subject_id: 2, tutor_id: 4)
+taughtlesson9.save
+taughtlesson10 = TaughtLesson.new(subject_id: 10, tutor_id: 4)
+taughtlesson10.save
+taughtlesson11 = TaughtLesson.new(subject_id: 2, tutor_id: 5)
+taughtlesson11.save
+puts 'Finished!'
+puts '-----------------------------'
+
+puts 'Creating Spoken Languages...'
+spoken1 = SpokenLanguages.new(language_id: 1, tutor_id: 1)
+spoken1.save
+spoken2 = SpokenLanguages.new(language_id: 3, tutor_id: 1)
+spoken2.save
+spoken3 = SpokenLanguages.new(language_id: 1, tutor_id: 2)
+spoken3.save
+spoken4 = SpokenLanguages.new(language_id: 3, tutor_id: 2)
+spoken4.save
+spoken5 = SpokenLanguages.new(language_id: 1, tutor_id: 3)
+spoken5.save
+spoken6 = SpokenLanguages.new(language_id: 3, tutor_id: 3)
+spoken6.save
+spoken7 = SpokenLanguages.new(language_id: 1, tutor_id: 4)
+spoken7.save
+spoken8 = SpokenLanguages.new(language_id: 3, tutor_id: 4)
+spoken8.save
+spoken9 = SpokenLanguages.new(language_id: 1, tutor_id: 5)
+spoken9.save
+spoken10 = SpokenLanguages.new(language_id: 4, tutor_id: 5)
+spoken10.save
 puts 'Finished!'
 puts '-----------------------------'
