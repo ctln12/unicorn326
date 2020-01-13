7b5f72daa06b05cc9a60eddab8cf99c8f2c57467
Cataline Baert <snuf_kin@yahoo.com>
2020-01-13 11:02:38 +0100

    add validations

--- app/models/spoken_language.rb
+++ app/models/spoken_language.rb
@@ -1,4 +1,7 @@
 class SpokenLanguage < ApplicationRecord
-  belongs_to :tutor
   belongs_to :language
+  belongs_to :tutor
+
+  validates :language_id, presence: true, uniqueness: { scope: :tutor_id }
+  validates :tutor_id, presence: true
 end
