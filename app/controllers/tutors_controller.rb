24d58dda137ae9063c006aa782bcd859ff338c9f
Cataline Baert <snuf_kin@yahoo.com>
2020-01-14 16:29:05 +0100

    create method profile

--- app/controllers/tutors_controller.rb
+++ app/controllers/tutors_controller.rb
@@ -1,5 +1,6 @@
 class TutorsController < ApplicationController
   before_action :authenticate_student!, only: [:show]
+  before_action :authenticate_tutor!, only: [:profile]
 
   def index
     if params[:subject_id].present?
@@ -26,5 +27,9 @@ class TutorsController < ApplicationController
 
   def show
     @tutor = Tutor.find(params[:id])
+  end
+
+  def profile
+    @tutor = current_tutor
   end
 end
