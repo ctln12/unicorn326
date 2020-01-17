affae18ad15e2e4223fcc92f517c0046d316751b
Cataline Baert <snuf_kin@yahoo.com>
2020-01-17 17:37:09 +0100

    add before action authenticate_student only for index and implement index method for students

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,6 @@
 class BookingsController < ApplicationController
+  before_action :authenticate_student!, only: :index
   def index
+    @bookings = Booking.where('student_id = ?', current_student.id)
   end
 end
