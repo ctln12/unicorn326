43a1473154c18df050d8567934809ed7cf33b344
Cataline Baert <snuf_kin@yahoo.com>
2020-01-17 15:15:44 +0100

    run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_17_104150) do
+ActiveRecord::Schema.define(version: 2020_01_17_141101) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +26,9 @@ ActiveRecord::Schema.define(version: 2020_01_17_104150) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.date "paid_at"
+    t.bigint "subject_id"
     t.index ["student_id"], name: "index_bookings_on_student_id"
+    t.index ["subject_id"], name: "index_bookings_on_subject_id"
     t.index ["tutor_id"], name: "index_bookings_on_tutor_id"
   end
 
@@ -108,6 +110,7 @@ ActiveRecord::Schema.define(version: 2020_01_17_104150) do
   end
 
   add_foreign_key "bookings", "students"
+  add_foreign_key "bookings", "subjects"
   add_foreign_key "bookings", "tutors"
   add_foreign_key "spoken_languages", "languages"
   add_foreign_key "spoken_languages", "tutors"
