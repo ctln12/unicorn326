f9381e4eea6f2759453951e5746a1e8dd06c8057
alessandro buccarelli <abuccarelli@hotmail.com>
2020-01-17 18:07:36 +0100

    added job post new

--- app/controllers/job_posts_controller.rb
+++ app/controllers/job_posts_controller.rb
@@ -8,7 +8,15 @@ class JobPostsController < ApplicationController
   end
 
   def create
-
+    @job_post = JobPost.new(job_post_params)
+    @job_post.student_id = current_student.id
+    @job_post.save
+    redirect_to job_posts_path
   end
 
+  private
+
+  def job_post_params
+    params.require(:job_post).permit(:title, :description, :currency, :amount, :subjects, :spoken_languages)
+  end
 end
--- app/models/job_post.rb
+++ app/models/job_post.rb
@@ -1,3 +1,10 @@
 class JobPost < ApplicationRecord
   belongs_to :student
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :currency, presence: true
+  validates :amount, presence: true
+  validates :subjects, presence: true
+  validates :spoken_languages, presence: true
 end
