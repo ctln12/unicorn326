b13a4612a32c1bb58b1399749d2144523fac597b
Cataline Baert <snuf_kin@yahoo.com>
2020-01-17 18:34:43 +0100

    add before action custom method that authenticates or redirects and implement method index for both tutors and students

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,8 +1,23 @@
 class BookingsController < ApplicationController
+  before_action :authenticate_or_redirect!, only: :index
+
   def index
     if current_tutor
       @bookings = Booking.where('tutor_id = ?', current_tutor.id)
     elsif current_student
       @bookings = Booking.where('student_id = ?', current_student.id)
     end
+  end
+
+  private
+
+  def authenticate_or_redirect!
+    if current_tutor
+      :authenticate_tutor!
+    elsif current_student
+      :authenticate_student!
+    else
+      redirect_to root_path
+    end
+  end
 end
