e9b5c0588cb1d120efe54c29a7cae5f0d008b203
Cataline Baert <snuf_kin@yahoo.com>
2020-01-17 11:48:13 +0100

    run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_17_092223) do
+ActiveRecord::Schema.define(version: 2020_01_17_104150) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_01_17_092223) do
     t.date "accepted_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "paid_at"
     t.index ["student_id"], name: "index_bookings_on_student_id"
     t.index ["tutor_id"], name: "index_bookings_on_tutor_id"
   end
