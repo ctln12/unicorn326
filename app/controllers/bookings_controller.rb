ede753086d484d6ef64d168a6d7cb1e4bba0c580
Cataline Baert <snuf_kin@yahoo.com>
2020-01-18 18:13:19 +0100

    implement method create the long way then comment and use strong params

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -10,10 +10,26 @@ class BookingsController < ApplicationController
   end
 
   def new
+    @student = current_student
+    @tutor = Tutor.find(params[:tutor_id])
     @booking = Booking.new
   end
 
+  def create
+    # date = DateTime.new(params[:booking]['date(1i)'].to_i, params[:booking]['date(2i)'].to_i, params[:booking]['date(3i)'].to_i, params[:booking]['date(4i)'].to_i, params[:booking]['date(5i)'].to_i)
+    # duration = params['duration(4i)'].to_i*60 + params['duration(5i)'].to_i
+    # @booking = Booking.new(student_id: params[:booking][:student_id].to_i, tutor_id: params[:booking][:tutor_id].to_i, subject_id: params[:booking][:subject_id].to_i, date: date, duration: duration, booking_price: params[:booking][:booking_price].to_f)
+    @booking = Booking.new(booking_params)
+    @booking.save
+
+    redirect_to bookings_path
+  end
+
   private
+
+  def booking_params
+    params.require(:booking).permit(:student_id, :tutor_id, :subject_id, :date, :duration, :booking_price)
+  end
 
   def redirect_if_user_not_signed_in!
     redirect_to root_path unless tutor_signed_in? || student_signed_in?
