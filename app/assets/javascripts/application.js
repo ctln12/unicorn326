0a01e8a910dd371095f37a83b3099d1435ceff04
Cataline Baert <snuf_kin@yahoo.com>
2020-01-19 21:46:14 +0100

    add change event on duration fields to update total price field

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,20 @@
 //= require rails-ujs
 //= require_tree .
+
+const hoursTag = document.querySelector('#booking_duration_4i');
+const minutesTag = document.querySelector('#booking_duration_5i');
+
+const pricePerHour = document.querySelector('#price_per_hour').value;
+
+let bookingPrice = document.querySelector('#booking_booking_price');
+
+const addChangeEvent = (htmlTag) => {
+  htmlTag.addEventListener('change', (event) => {
+    const hours = hoursTag.value;
+    const minutes = minutesTag.value;
+    bookingPrice.value = parseFloat(pricePerHour) * (parseFloat(hours) + parseFloat(minutes) / 60);
+  });
+};
+
+addChangeEvent(hoursTag);
+addChangeEvent(minutesTag);
