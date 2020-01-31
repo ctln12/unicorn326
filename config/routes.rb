c5e967b6528af2a00ab04e320aae5132acf438d4
Cataline Baert <snuf_kin@yahoo.com>
2020-01-31 18:23:14 +0100

    add show, edit and update routes

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,6 @@ Rails.application.routes.draw do
   resources :languages, :only => [:index]
   resources :taught_lessons, :only => [:index, :new, :create, :destroy]
   resources :spoken_languages, :only => [:index, :new, :create, :destroy]
-  resources :bookings, :only => [:index, :new, :create]
+  resources :bookings, :only => [:index, :new, :create, :show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
