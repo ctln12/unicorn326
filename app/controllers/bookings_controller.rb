db88d43e92fc970fcdf368f357938e30baf86821
alessandro buccarelli <abuccarelli@hotmail.com>
2020-02-12 23:31:34 +0100

    updated booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -61,7 +61,7 @@ class BookingsController < ApplicationController
       success_url: booking_url(@booking),
       cancel_url: booking_url(@booking)
     )
-    @booking.update(checkout_session_id: session.id, go_payment: false, paid_at: DateTime.now)
+    @booking.update(checkout_session_id: session.id, go_payment: false)
     redirect_to new_booking_payment_path(@booking)
   end
 
