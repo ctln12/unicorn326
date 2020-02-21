5e75a164bebb9604789f0314107a4c54e7364385
Cataline Baert <snuf_kin@yahoo.com>
2020-02-21 17:41:17 +0100

    move script for video using agora to other folder

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,2 @@
 //= require rails-ujs
 //= require_tree .
-
-// Add change event on start date
-if (startDay || startMonth || startYear || startHour || startMinute) {
-  startDay.addEventListener('change', (event) => update(endDay));
-  startMonth.addEventListener('change', (event) => update(endMonth));
-  startYear.addEventListener('change', (event) => update(endYear));
-  startHour.addEventListener('change', (event) => update(endHour));
-  startMinute.addEventListener('change', (event) => update(endMinute));
-}
-// Add change event on end date (only time)
-if (endHour || endMinute) {
-  endHour.addEventListener('change', totalPrice);
-  endMinute.addEventListener('change', totalPrice);
-}
