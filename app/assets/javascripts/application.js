af30260df6e61de8449c402453c630b9acb81e72
Cataline Baert <snuf_kin@yahoo.com>
2020-02-25 18:33:41 +0100

    move script from folder to application file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,18 @@
 //= require rails-ujs
 //= require_tree .
+
+// ----------------- BOOKINGS -----------------
+// Add change event on start date
+if (startDay || startMonth || startYear || startHour || startMinute) {
+  startDay.addEventListener("change", event => update(endDay));
+  startMonth.addEventListener("change", event => update(endMonth));
+  startYear.addEventListener("change", event => update(endYear));
+  startHour.addEventListener("change", event => update(endHour));
+  startMinute.addEventListener("change", event => update(endMinute));
+}
+
+// Add change event on end date (only time)
+if (endHour || endMinute) {
+  endHour.addEventListener("change", totalPrice);
+  endMinute.addEventListener("change", totalPrice);
+}
