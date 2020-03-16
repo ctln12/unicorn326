1b33569878db9dc577e2d53adc7094a407b0ca80
Cataline Baert <snuf_kin@yahoo.com>
2020-03-16 23:35:48 +0100

    define method create

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,21 @@
 class MessagesController < ApplicationController
+  before_action :load_entities
+
+  def create
+    @messages = @chat.messages
+
+    @username = current_student.first_name || current_tutor.first_name
+
+    @message = Message.create(content: params.dig(:message, :content), username: @username, chat: @chat)
+  end
+
+  protected
+
+  def load_entities
+    @chat = Chat.find(params.dig(:message, :chat_id))
+  end
+
+  def messages_params
+    params.require(:chat).permit(:content, :username, :chat_id)
+  end
 end
