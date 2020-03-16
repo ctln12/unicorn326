d20fcd17dd4351dac07754052d579d9617910669
Cataline Baert <snuf_kin@yahoo.com>
2020-03-16 23:35:19 +0100

    define methods index, new, create and show

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,10 +1,41 @@
 class ChatsController < ApplicationController
   before_action :redirect_if_user_not_signed_in!, only: :index
+  before_action :load_entities
 
   def index
+    @chats = Chat.all
   end
 
-  private
+  def new
+    @chat = Chat.new
+  end
+
+  def create
+    @chat = Chat.new(chats_params)
+
+    if @chat.save
+      flash[:success] = "Room was created successfully"
+      redirect_to chats_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @message = Message.new(chat: @chat)
+    @messages = @chat.messages
+  end
+
+  protected
+
+  def load_entities
+    @chats = Chat.all
+    @chat = Chat.find(params[:id]) if params[:id]
+  end
+
+  def chats_params
+    params.require(:chat).permit(:student_id, :tutor_id)
+  end
 
   def redirect_if_user_not_signed_in!
     redirect_to root_path unless tutor_signed_in? || student_signed_in?
