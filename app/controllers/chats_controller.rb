df9989647141b05f0766b320ba7bf4b848c1f9a0
Cataline Baert <snuf_kin@yahoo.com>
2020-03-16 20:39:51 +0100

    define method index and create view

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,2 +1,12 @@
 class ChatsController < ApplicationController
+  before_action :redirect_if_user_not_signed_in!, only: :index
+
+  def index
+  end
+
+  private
+
+  def redirect_if_user_not_signed_in!
+    redirect_to root_path unless tutor_signed_in? || student_signed_in?
+  end
 end
