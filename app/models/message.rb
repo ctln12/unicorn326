3cf72c41bdfe291036e2dadf79da2bc5f48525f6
Cataline Baert <snuf_kin@yahoo.com>
2020-03-17 03:44:58 +0100

    define new method that changes the json representation of the new message to include an avatar image

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,7 @@
 class Message < ApplicationRecord
   belongs_to :chat
+
+  def as_json(options)
+    super(options).merge(user_avatar_url: "https://kitt.lewagon.com/placeholder/users/ssaunier")
+  end
 end
