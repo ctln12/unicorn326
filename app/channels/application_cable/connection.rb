0283d0048fdbe4e711c9315f712a4cddb0d22130
Cataline Baert <snuf_kin@yahoo.com>
2020-03-17 03:39:17 +0100

    modify websocket connection to work for both users

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,19 +1,32 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
-    identified_by :current_user
+    identified_by :current_tutor, :current_student
 
     def connect
-      self.current_user = find_verified_user
+      if current_student
+        self.current_student = find_verified_student
+      elsif current_tutor
+        self.current_tutor = find_verified_tutor
+      end
     end
 
     private
 
-    def find_verified_user
-      if verified_user = User.find_by(id: cookies.signed['user.id'])
-        verified_user
+    def find_verified_student
+      if verified_student = Student.find_by(id: cookies.signed['student.id'])
+        verified_student
       else
         reject_unauthorized_connection
       end
     end
+
+    def find_verified_tutor
+      if verified_tutor = Tutor.find_by(id: cookies.signed['tutor.id'])
+        verified_tutor
+      else
+        reject_unauthorized_connection
+      end
+    end
+
   end
 end
