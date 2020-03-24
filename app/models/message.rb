9093e811608ea29b70ccaea0a87886e3ae80a214
Cataline Baert <snuf_kin@yahoo.com>
2020-03-24 23:17:46 +0100

    add validations

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,10 @@
 class Message < ApplicationRecord
   belongs_to :chat
 
+  validates :content, presence: true
+  validates :is_student, inclusion: { in: [ true, false ] }
+  validates :chat_id, presence: true
+
   def as_json(options)
     super(options).merge(user_avatar_url: "https://kitt.lewagon.com/placeholder/users/ssaunier")
   end
