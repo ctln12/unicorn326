a762d373edd6bd3af8d1bc8d75fb1735d03bd504
Cataline Baert <snuf_kin@yahoo.com>
2020-03-24 23:14:49 +0100

    remove username column and add column is student to messages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_16_184251) do
+ActiveRecord::Schema.define(version: 2020_03_24_221218) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -119,10 +119,10 @@ ActiveRecord::Schema.define(version: 2020_03_16_184251) do
 
   create_table "messages", force: :cascade do |t|
     t.text "content"
-    t.string "username"
     t.bigint "chat_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_student"
     t.index ["chat_id"], name: "index_messages_on_chat_id"
   end
 
