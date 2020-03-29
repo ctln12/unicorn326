8876116b5c8ea35154f39220177a813b973cc478
alessandro buccarelli <abuccarelli@hotmail.com>
2020-03-29 16:28:05 +0200

    sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['USERNAME_SENDGRID'],
+  :password => ENV['PASSWORD_SENDGRID'],
+  :domain => 'unicorn326.herokuapp.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 465,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
