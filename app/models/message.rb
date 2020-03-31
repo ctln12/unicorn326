b69c57fdfbd0471dc14bc10ddb59a15ed1f43464
Cataline Baert <snuf_kin@yahoo.com>
2020-04-01 01:31:25 +0200

    create model method author to retrieve the author of a message more easily and add validation for author id column"

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,4 +4,15 @@ class Message < ApplicationRecord
   validates :content, presence: true
   validates :is_student, inclusion: { in: [ true, false ] }
   validates :chat_id, presence: true
+  validates :author_id, presence: true
+
+  def author
+    unless is_student == nil
+      if is_student
+        Student.find(author_id)
+      else
+        Tutor.find(author_id)
+      end
+    end
+  end
 end
