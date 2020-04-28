030fb86fa54ba91a5dde32ddf6d8ac146cbe0f5a
alessandro buccarelli <abuccarelli@hotmail.com>
2020-04-28 02:21:13 +0200

    added uniqueness of the review

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,5 @@ class Review < ApplicationRecord
   # belongs_to :student
   validates :comment, length: { minimum: 20 }, presence: true
   validates :rating, presence: true
+  validates :booking_id, presence: true, uniqueness: true
 end
