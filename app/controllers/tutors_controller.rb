6e17fe849427aa94214c9338f5a1f7280befbbca
Cataline Baert <snuf_kin@yahoo.com>
2020-08-22 13:40:19 +0200

    refactor search in index method of tutors controller

--- app/controllers/tutors_controller.rb
+++ app/controllers/tutors_controller.rb
@@ -3,42 +3,18 @@ class TutorsController < ApplicationController
   before_action :authenticate_tutor!, only: [:profile]
 
   def index
-    if params[:subject_id].nil? && params[:language_id].nil? && params[:country].nil?
-      @tutors = Tutor.all
-    else
-      subject = Subject.find(params[:subject_id].to_i).name unless params[:subject_id] == ''
-      language = Language.find(params[:language_id].to_i).name unless params[:language_id] == ''
-      country_code = ISO3166::Country.find_country_by_name(params[:country]).alpha2 unless params[:country] == ''
-      if params[:subject_id] == '' && params[:language_id] == '' && params[:country] == ''
-        @tutors = Tutor.all
-      elsif params[:language_id] == '' && params[:country] == ''
-        @tutors = Tutor
-                  .joins(:subjects)
-                  .where(['subjects.name = ?', subject])
-      elsif params[:subject_id] == '' && params[:country] == ''
-        @tutors = Tutor
-                  .joins(:languages)
-                  .where(['languages.name = ?', language])
-      elsif params[:subject_id] == '' && params[:language_id] == ''
-        @tutors = Tutor
-                  .where(['country = ?', country_code])
-      elsif params[:country] == ''
-        @tutors = Tutor
-                  .joins(:subjects, :languages)
-                  .where(['subjects.name = ? and languages.name = ?', subject, language])
-      elsif params[:language_id] == ''
-        @tutors = Tutor
-                  .joins(:subjects)
-                  .where(['subjects.name = ? and country = ?', subject, country_code])
-      elsif params[:subject_id] == ''
-        @tutors = Tutor
-                  .joins(:languages)
-                  .where(['languages.name = ? and country = ?', language, country_code])
-      else
-        @tutors = Tutor
-                  .joins(:subjects, :languages)
-                  .where(['subjects.name = ? and languages.name = ? and country = ?', subject, language, country_code])
+    if params[:subject].present? || params[:language].present? || params[:country].present?
+      sql_queries = []
+      sql_queries << "subjects.name = :subject" unless params[:subject].empty?
+      sql_queries << "languages.name = :language" unless params[:language].empty?
+      unless params[:country].empty?
+        country_code = ISO3166::Country.find_country_by_name(params[:country]).alpha2
+        sql_queries << "tutors.country = :country"
       end
+      sql_query = sql_queries.join(" AND ")
+      @tutors = Tutor.joins(:subjects).joins(:languages).where(sql_query, subject: params[:subject], language: params[:language], country: country_code).uniq
+    else
+      @tutors = Tutor.all
     end
     @tutors_nb = @tutors.count
   end
