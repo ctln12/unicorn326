3723c6e186d75e1dd7dcef7dc8ec15565cbd210d
Cataline Baert <snuf_kin@yahoo.com>
2020-08-25 18:23:29 +0200

    implement instantsearch in tutors index page, add js and css

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,22 +1,69 @@
 import "bootstrap";
 import "bootstrap-select";
-import { initOpenTok } from "./lesson";
-import algoliasearch from "algoliasearch";
 
 require('dotenv').config()
 
+import { initOpenTok } from "./lesson";
+
 initOpenTok();
 
+import algoliasearch from "algoliasearch";
+import instantsearch from "instantsearch.js";
+import { searchBox, hits } from "instantsearch.js/es/widgets";
+
 const tutors = document.getElementById('tutors');
+
 if (tutors) {
-  var client = algoliasearch("11UNBLSZP5", "1beb4d87ba411f5793a0645f224e90ce");
-  var index = client.initIndex("Tutor");
-  // index
-  //   .search("Deandre", { hitsPerPage: 10, page: 0 })
-  //   .then(function searchDone(content) {
-  //     console.log(content);
-  //   })
-  //   .catch(function searchFailure(err) {
-  //     console.error(err);
-  //   });
+  const searchClient = algoliasearch("11UNBLSZP5", "1beb4d87ba411f5793a0645f224e90ce");
+
+  const search = instantsearch({
+    indexName: "Tutor",
+    searchClient,
+  });
+
+  search.addWidgets([
+    searchBox({
+      container: "#searchbox",
+    }),
+
+    hits({
+      container: "#hits",
+      templates: {
+        item: `
+        <div>
+          <img src="{{image}}" align="left" alt="{{name}}" />
+          <div class="hit-name">
+            <h4>
+              {{#helpers.highlight}}{ "attribute": "first_name" }{{/helpers.highlight}}
+              {{#helpers.highlight}}{ "attribute": "last_name" }{{/helpers.highlight}}
+            </h4>
+          </div>
+          <div class="hit-country">
+            <p>{{#helpers.highlight}}{ "attribute": "country" }{{/helpers.highlight}}</p>
+          </div>
+          <div class="hit-price">
+            {{#helpers.highlight}}{ "attribute": "currency" }{{/helpers.highlight}}
+            {{#helpers.highlight}}{ "attribute": "price" }{{/helpers.highlight}}
+          </div>
+          <div class="hit-subjects">
+            {{#subjects}}
+              <span>{{name}}</span>
+            {{/subjects}}
+          </div>
+          <div class="hit-languages">
+            {{#languages}}
+              <span>{{name}}</span>
+            {{/languages}}
+          </div>
+          <div class="hit-rating">
+            {{#helpers.highlight}}{ "attribute": "average_rating" }{{/helpers.highlight}}
+          </div>
+          <a href='tutors/{{id}}' class='btn btn-primary'>View profile</a>
+        </div>
+      `,
+      },
+    }),
+  ]);
+
+  search.start();
 }
