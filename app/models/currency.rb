3eda783dd70c4e4b85f9022b66d172e2dda6d1a5
Cataline Baert <snuf_kin@yahoo.com>
2020-08-25 15:10:18 +0200

    configure tutor and currency models to use algoliasearch

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -2,4 +2,6 @@ class Currency < ApplicationRecord
   has_many :tutors
   has_many :job_posts
   has_many :wallets, through: :tutors
+
+  after_save { tutors.each(&:touch) }
 end
--- app/models/tutor.rb
+++ app/models/tutor.rb
@@ -1,18 +1,19 @@
 class Tutor < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
   has_many :taught_lessons
   has_many :spoken_languages
   has_many :bookings
-  has_one :currency
+  # has_one :currency
+  belongs_to :currency
   has_many :subjects, through: :taught_lessons
   has_many :languages, through: :spoken_languages
   has_many :reviews, through: :bookings
   has_many :wallets
   has_many :chats
-
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
 
   validates :first_name, presence: true
   validates :last_name, presence: true
@@ -26,6 +27,36 @@ class Tutor < ApplicationRecord
   validates :price, presence: true
 
   after_create :send_welcome_email
+
+  after_validation :update_attributes
+
+  include AlgoliaSearch
+  algoliasearch do
+    attributes :id, :first_name, :last_name, :email, :price
+    attribute :country do
+      ISO3166::Country.find_country_by_alpha2(self.country).unofficial_names.first
+    end
+    attribute :currency do
+      currency.name
+    end
+    attribute :languages do
+      languages.select { |s| s }.map do |s|
+        { name: s.name }
+      end
+    end
+    attribute :subjects do
+      subjects.select { |s| s }.map do |s|
+        { name: s.name }
+      end
+    end
+    attribute :rating do
+      reviews.select { |s| s }.map do |s|
+        { rating: s.rating }
+      end
+    end
+    searchableAttributes ['subjects', 'languages', 'country', 'price', 'rating', 'currency', 'unordered(first_name)', 'unordered(last_name)']
+    customRanking ['asc(price)']
+  end
 
   private
 
