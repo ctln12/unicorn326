186e795dc82809903d01b5df0d41ed595054019c
Cataline Baert <snuf_kin@yahoo.com>
2020-08-28 17:53:25 +0200

    modify style of more buttons, cards, sort by button

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -31,41 +31,43 @@ if (tutors) {
       container: "#hits",
       templates: {
         item: `
-        <div>
-          <img src="{{image}}" align="left" alt="{{name}}" />
-          <div class="hit-name">
-            <h4>
+        <div class="card">
+          <img class="card-img-top" src="https://www.lifewire.com/thmb/xVByvioteKa61v_yrpENqbYROWI=/1300x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/find-anyone-online-3482687-91a015d74d794f77b299703eff0f47a9.png" align="left" alt="tutor's photo">
+          <div class="card-body">
+            <h4 class="card-title hit-name">
               {{first_name}}
               {{last_name}}
             </h4>
+            <p class="card-text hit-country">{{country}}</p>
+            <p class="card-text hit-price">
+              {{currency}}
+              {{price}}
+            </p>
+            <p class="card-text hit-subjects">
+              Teaches:
+              {{#subjects}}
+                <span>{{name}}</span>
+              {{/subjects}}
+            </p>
+            <p class="card-text hit-languages">
+              Speaks:
+              {{#languages}}
+                <span>{{name}}</span>
+              {{/languages}}
+            </p>
+            <p class="card-text hit-rating">
+              Rating:
+              {{average_rating}}
+            </p>
+            <a href='tutors/{{id}}' class='btn btn-primary'>View profile</a>
           </div>
-          <div class="hit-country">
-            {{country}}
-          </div>
-          <div class="hit-price">
-            {{currency}}
-            {{price}}
-            / h
-          </div>
-          <div class="hit-subjects">
-            Teaches:
-            {{#subjects}}
-              <span>{{name}}</span>
-            {{/subjects}}
-          </div>
-          <div class="hit-languages">
-            Speaks:
-            {{#languages}}
-              <span>{{name}}</span>
-            {{/languages}}
-          </div>
-          <div class="hit-rating">
-            Rating:
-            {{average_rating}}
-          </div>
-          <a href='tutors/{{id}}' class='btn btn-primary'>View profile</a>
         </div>
       `,
+      },
+      cssClasses: {
+        root: 'search-results',
+        list: 'tutors-list row',
+        item: 'tutor col-12 col-sm-6',
       },
     }),
 
@@ -75,17 +77,29 @@ if (tutors) {
 
     currentRefinements({
       container: '#current-filters',
+      cssClasses: {
+        item: 'filter-tag',
+        label: 'dark-blue-color',
+        categoryLabel: 'dark-blue-color',
+        delete: 'dark-blue-color'
+      }
     }),
 
     clearRefinements({
       container: "#clear-filters",
       templates: {
-        resetLabel: 'Clear all',
+        resetLabel: 'Clear filters',
       },
+      cssClasses: {
+        button: 'dark-blue-color'
+      }
     }),
 
     sortBy({
       container: '#sort-by',
+      cssClasses: {
+        select: 'custom-select'
+      },
       items: [
         { label: 'Most relevant', value: 'Tutor_development' },
         // { label: 'Popularity', value: 'Tutor_by_rating_desc_development' },
@@ -99,6 +113,21 @@ if (tutors) {
       attribute: 'subjects.name',
       limit: 3,
       showMore: true,
+      templates: {
+        showMoreText: `
+        {{#isShowingMore}}
+          <i class="fas fa-chevron-up"></i>
+          less
+        {{/isShowingMore}}
+        {{^isShowingMore}}
+          <i class="fas fa-chevron-down"></i>
+          more
+        {{/isShowingMore}}
+        `,
+      },
+      cssClasses: {
+        showMore: 'more-button',
+      }
     }),
 
     refinementList({
@@ -106,6 +135,21 @@ if (tutors) {
       attribute: 'languages.name',
       limit: 3,
       showMore: true,
+      templates: {
+        showMoreText: `
+        {{#isShowingMore}}
+          <i class="fas fa-chevron-up"></i>
+          less
+        {{/isShowingMore}}
+        {{^isShowingMore}}
+          <i class="fas fa-chevron-down"></i>
+          more
+        {{/isShowingMore}}
+        `,
+      },
+      cssClasses: {
+        showMore: 'more-button',
+      }
     }),
 
     refinementList({
@@ -113,6 +157,21 @@ if (tutors) {
       attribute: 'country',
       limit: 3,
       showMore: true,
+      templates: {
+        showMoreText: `
+        {{#isShowingMore}}
+          <i class="fas fa-chevron-up"></i>
+          less
+        {{/isShowingMore}}
+        {{^isShowingMore}}
+          <i class="fas fa-chevron-down"></i>
+          more
+        {{/isShowingMore}}
+        `,
+      },
+      cssClasses: {
+        showMore: 'more-button',
+      }
     }),
 
     refinementList({
@@ -120,6 +179,21 @@ if (tutors) {
       attribute: 'currency',
       limit: 3,
       showMore: true,
+      templates: {
+        showMoreText: `
+        {{#isShowingMore}}
+          <i class="fas fa-chevron-up"></i>
+          less
+        {{/isShowingMore}}
+        {{^isShowingMore}}
+          <i class="fas fa-chevron-down"></i>
+          more
+        {{/isShowingMore}}
+        `,
+      },
+      cssClasses: {
+        showMore: 'more-button',
+      }
     }),
 
     pagination({
@@ -131,3 +205,38 @@ if (tutors) {
 
   search.start();
 }
+
+/* <div>
+  <img src="https://www.lifewire.com/thmb/xVByvioteKa61v_yrpENqbYROWI=/1300x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/find-anyone-online-3482687-91a015d74d794f77b299703eff0f47a9.png" align="left" class="tutor-image" alt="photo" />
+  <div class="hit-name">
+    <h4>
+      {{ first_name }}
+      {{ last_name }}
+    </h4>
+  </div>
+  <div class="hit-country">
+    {{ country }}
+  </div>
+  <div class="hit-price">
+    {{ currency }}
+    {{ price }}
+            / h
+          </div>
+  <div class="hit-subjects">
+    Teaches:
+            {{ #subjects}}
+    <span>{{ name }}</span>
+    {{/ subjects}}
+          </div>
+  <div class="hit-languages">
+    Speaks:
+            {{ #languages}}
+    <span>{{ name }}</span>
+    {{/ languages}}
+          </div>
+  <div class="hit-rating">
+    Rating:
+            {{ average_rating }}
+  </div>
+  <a href='tutors/{{id}}' class='btn btn-primary'>View profile</a>
+</div> */
