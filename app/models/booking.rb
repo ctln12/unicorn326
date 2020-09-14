caef09c7a8f2d992925e844bb3ab4f75d6ee59e8
Cataline Baert <snuf_kin@yahoo.com>
2020-09-14 17:49:11 +0200

    change booking model's validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -7,5 +7,5 @@ class Booking < ApplicationRecord
   has_many :reviews
   has_one :lesson
 
-  validates :start_date, :end_date, :booking_price, presence: true
+  validates :date, :start_time, :end_time, :booking_price, presence: true
 end
