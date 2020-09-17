07deac764f53296a8d38ddef6509a7385ce4a9cc
Cataline Baert <snuf_kin@yahoo.com>
2020-09-17 19:46:49 +0200

    create status method of Booking model, use it in bookings index view and style

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -8,4 +8,20 @@ class Booking < ApplicationRecord
   has_one :lesson, dependent: :destroy
 
   validates :date, :start_time, :end_time, :booking_price, presence: true
+
+  def status
+    return {class: 'canceled', message: 'CANCELED'} if self.canceled_at
+
+    if self.paid_at && self.date < Date.today
+      {class: 'completed', message: 'COMPLETED'}
+    elsif self.paid_at && self.date == Date.today
+      {class: 'today', message: 'TODAY'}
+    elsif self.paid_at && self.date > Date.today
+      {class: 'coming', message: 'COMING SOON'}
+    elsif self.accepted_at
+      {class: 'unpaid', message: 'PAYMENT PENDING'}
+    else
+      {class: 'unconfirmed', message: 'CONFIRMATION PENDING'}
+    end
+  end
 end
