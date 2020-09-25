b7321d8645e3ec041b02f61cec8a17f1876a9724
Cataline Baert <snuf_kin@yahoo.com>
2020-09-25 18:52:25 +0200

    remove validations for opentok_token and opentok_session_id, add before create method that sets an opentok session id to a lesson

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,6 +2,14 @@ class Lesson < ApplicationRecord
   belongs_to :booking
 
   validates :booking_id, presence: true, uniqueness: true
-  validates :opentok_session_id, presence: true
-  validates :opentok_token, presence: true
+
+  before_create :set_opentok_session_id
+
+  private
+
+  def set_opentok_session_id
+    opentok = OpenTok::OpenTok.new(ENV['OPENTOK_API_KEY'], ENV['OPENTOK_SECRET_KEY'])
+    session = opentok.create_session
+    self.opentok_session_id = session.session_id
+  end
 end
