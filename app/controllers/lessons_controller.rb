29b7aa323ae10de4a09ecee37c8ef4b32b27fb12
Cataline Baert <snuf_kin@yahoo.com>
2020-09-25 19:22:50 +0200

    refactor show method and simplify create method from lessons controller

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,44 +1,24 @@
-require 'opentok'
-
 class LessonsController < ApplicationController
-    before_action :config_opentok, only: :create
-
   def create
     @booking = Booking.find(params[:booking_id])
 
     if @booking.lesson
       redirect_to booking_lesson_path(@booking, @booking.lesson)
     else
-      @lesson = Lesson.new()
+      @lesson = Lesson.new
       @lesson.booking = @booking
-      session = @opentok.create_session
-      opentok_session_id = session.session_id
-      @lesson.opentok_session_id = opentok_session_id
-      opentok_token = @opentok.generate_token(opentok_session_id)
-      @lesson.opentok_token = opentok_token
       if @lesson.save
         redirect_to booking_lesson_path(@booking, @lesson)
       else
-        render "bookings/show"
+        redirect_to booking_path(@booking)
       end
     end
   end
 
   def show
     @lesson = Lesson.find(params[:id])
-    @opentok_session_id = @lesson.opentok_session_id
-    @opentok_token = @lesson.opentok_token
-  end
 
-  private
-
-  def config_opentok
-    if @opentok.nil?
-      @opentok = OpenTok::OpenTok.new(ENV['OPENTOK_API_KEY'], ENV['OPENTOK_SECRET_KEY'])
-    end
-  end
-
-  def lesson_params
-    params.require(:lesson).permit(:video_url, :booking_id)
+    opentok = OpenTok::OpenTok.new(ENV['OPENTOK_API_KEY'], ENV['OPENTOK_SECRET_KEY'])
+    @opentok_token = opentok.generate_token(@lesson.opentok_session_id)
   end
 end
