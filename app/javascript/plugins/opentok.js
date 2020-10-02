f00a57c3989d9ca3931cecdd9ef75c2e9ccf4996
Cataline Baert <snuf_kin@yahoo.com>
2020-10-02 18:48:55 +0200

    refactor code and add screen-sharing

--- app/javascript/plugins/opentok.js
+++ app/javascript/plugins/opentok.js
@@ -1,23 +1,186 @@
 const initOpentok = () => {
   const videosContainer = document.getElementById("videos");
 
+  const connectToSession = (token) => {
+    session.on({
+      connectionCreated: function (event) {
+        connectionCount++;
+        console.log(connectionCount + ' connections.');
+      },
+      connectionDestroyed: function (event) {
+        connectionCount--;
+        console.log(connectionCount + ' connections.');
+      },
+      sessionDisconnected: function sessionDisconnectHandler(event) {
+        console.log('Disconnected from the session.');
+        document.getElementById('disconnectBtn').style.display = 'none';
+        if (event.reason == 'networkDisconnected') {
+          alert('Your network connection terminated.')
+        }
+      },
+      streamCreated: function(event) {
+        console.log("New stream in the session: " + event.stream.streamId);
+        var subscriber;
+        console.log("Video type: ", event.stream.videoType);
+        if (event.stream.videoType === 'screen') {
+          subscriber = session.subscribe(
+            event.stream,
+            'screen-preview',
+            {
+              insertMode: 'append',
+              width: "100%",
+              height: "100%"
+            }, function (error) {
+            if (error) {
+              console.log("Error: ", error);
+            } else {
+              console.log('Subscriber to screen-sharing added.');
+              const subscriberContainer = document.getElementById("subscriber");
+              const screenPreviewContainer = document.getElementById("screen-preview");
+              if (screenPreviewContainer.children[0].classList.contains("OT_subscriber")) {
+                subscriberContainer.classList.add("subscriber-small");
+                console.log("Show subscriber in small window.");
+              }
+            }
+          });
+        } else {
+          subscriber = session.subscribe(
+            event.stream,
+            'subscriber',
+            {
+              insertMode: 'append',
+              width: "100%",
+              height: "100%"
+            }, function (error) {
+            if (error) {
+              console.log("Error: ", error);
+            } else {
+              console.log('Subscriber to video stream added.');
+            }
+          });
+        }
+        subscriber.setStyle({ buttonDisplayMode: 'off' });
+      },
+      streamDestroyed: function(event) {
+        console.log("Stream destroyed. Reason: ", event.reason);
+        const subscriberContainer = document.getElementById("subscriber");
+          subscriberContainer.classList.remove("subscriber-small");
+          console.log("Show subscriber in big window.");
+      }
+    });
+    publisher = OT.initPublisher(
+      'publisher',
+      {
+        style: { buttonDisplayMode: 'off' },
+        insertMode: 'append',
+        width: "100%",
+        height: "100%"
+      });
+    publisher.on({
+      streamCreated: function (event) {
+        console.log("Publisher started streaming.");
+        muteButton.addEventListener("click", () => {
+          if (publisher.stream.hasAudio) {
+            publisher.publishAudio(false);
+            muteButton.innerHTML = "<i class='fas fa-microphone'></i>";
+            console.log("Audio off");
+          } else {
+            publisher.publishAudio(true);
+            muteButton.innerHTML = "<i class='fas fa-microphone-slash'></i>";
+            console.log("Audio on");
+          }
+        });
+        hideVideoButton.addEventListener("click", () => {
+          if (publisher.stream.hasVideo) {
+            publisher.publishVideo(false);
+            hideVideoButton.innerHTML = "<i class='fas fa-video'></i>";
+            console.log("Video off");
+          } else {
+            publisher.publishVideo(true);
+            hideVideoButton.innerHTML = "<i class='fas fa-video-slash'></i>";
+            console.log("Video on");
+          }
+        });
+      },
+      streamDestroyed: function (event) {
+        console.log("Publisher stopped streaming. Reason: "
+          + event.reason);
+      }
+    });
+    session.connect(token, function (error) {
+      if (error) {
+        console.log("Error connecting: ", error.name, error.message);
+      } else {
+        console.log("Connected to the session.");
+        session.publish(publisher);
+      }
+    });
+  };
+
+  const stopPublishing = () => {
+    session.unpublish(publisher);
+  };
+
+  const screenSharing = () => {
+    OT.checkScreenSharingCapability(function (response) {
+      if (!response.supported || response.extensionRegistered === false) {
+        alert("This browser does not support screen sharing.");
+      } else if (response.extensionInstalled === false) {
+        alert("Please install the extension.");
+      } else {
+        screenSharingPublisher = OT.initPublisher(
+          'screen-preview',
+          {
+            videoSource: 'screen',
+            insertMode: 'append',
+            width: 360,
+            height: 240
+          },function (error) {
+            if (error) {
+              console.log("Error: ", error.message);
+            } else {
+              session.publish(screenSharingPublisher, function (error) {
+                if (error) {
+                  console.log("Error: ", error.message);
+                } else {
+                  console.log("Screen-sharing published.");
+                }
+              });
+            }
+          }
+        );
+        screenSharingPublisher.on('mediaStopped', function (event) {
+          console.log("Screen-sharing stopped.");
+        });
+      }
+    });
+  };
+
   if (videosContainer) {
-    const startCallButton = document.getElementById("start-call");
     const callButtonsContainer = document.getElementById("call-buttons");
+    const startCallButton = document.getElementById("start-call");
     const endCallButton = document.getElementById("end-call");
-    var timer;
+    const screenSharingButton = document.getElementById("screen-sharing");
 
     const opentokApiKey = process.env.OPENTOK_API_KEY;
-    const opentokSessionId = videosContainer.dataset.opentokSessionId;
-    const opentokToken = videosContainer.dataset.opentokToken;
+    const opentokSessionID = videosContainer.dataset.opentokSessionId;
+    const opentokToken = videosContainer.dataset.opentokToken; // one per person
 
-    // Event listeners
+    var session = OT.initSession(opentokApiKey, opentokSessionID); // one per lesson
+    console.log("Session ID: ", session.id);
+    var connectionCount = 0;
+    var publisher;
+    var screenSharingPublisher;
+    var timer;
+    var muteButton = document.getElementById("mute");
+    var hideVideoButton = document.getElementById("hide-video");
+
     videosContainer.addEventListener("mousemove", () => {
-        callButtonsContainer.removeAttribute("hidden");
-        clearTimeout(timer);
-        timer = setTimeout(function () {
-          callButtonsContainer.setAttribute("hidden", "");
-        }, 750);
+      callButtonsContainer.removeAttribute("hidden");
+      clearTimeout(timer);
+      timer = setTimeout(function () {
+        callButtonsContainer.setAttribute("hidden", "");
+      }, 750);
     });
     callButtonsContainer.addEventListener("mouseover", (event) => {
       event.currentTarget.removeAttribute("hidden");
@@ -26,92 +189,17 @@ const initOpentok = () => {
     startCallButton.addEventListener("click", (event) => {
       event.currentTarget.setAttribute("hidden", "");
       videosContainer.removeAttribute("hidden");
-      // initializeSession();
+      connectToSession(opentokToken);
     });
     endCallButton.addEventListener("click", (event) => {
       callButtonsContainer.setAttribute("hidden", "");
-      startCallButton.removeAttribute("hidden");
       videosContainer.setAttribute("hidden", "");
-      // disconnect();
+      stopPublishing();
     });
-
-    // Screen-sharing
-    function screenSharing() {
-      OT.checkScreenSharingCapability(function (response) {
-        if (!response.supported || response.extensionRegistered === false) {
-          // This browser does not support screen sharing
-          handleError;
-        } else if (response.extensionInstalled === false) {
-          // Prompt to install the extension.
-          alert("Please install the extension for screen-sharing.");
-        } else {
-          // Screen sharing is available. Publish the screen.
-          console.log("Start screen-sharing.");
-          var publisher = OT.initPublisher(
-            "screen-preview",
-            { videoSource: "screen" },
-            function (error) {
-              if (error) {
-                // Look at error.message to see what went wrong.
-                handleError(error);
-              } else {
-                session.publish(publisher, function (error) {
-                  if (error) {
-                    // Look error.message to see what went wrong.
-                    handleError(error);
-                  }
-                });
-              }
-            }
-          );
-        }
-      });
-    }
-
-    // Handling all of our errors here by alerting them
-    function handleError(error) {
-      if (error) {
-        alert(error.message);
-      }
-    }
-
-    function initializeSession() {
-      var session = OT.initSession(opentokApiKey, opentokSessionId);
-
-      // Subscribe to a newly created stream
-      session.on('streamCreated', function (event) {
-        session.subscribe(event.stream, 'subscriber', {
-          insertMode: 'append',
-          width: '100%',
-          height: '100%'
-        }, handleError);
-      });
-
-      // Create a publisher
-      var publisher = OT.initPublisher('publisher', {
-        insertMode: 'append',
-        width: '100%',
-        height: '100%'
-      }, handleError);
-
-      // Connect to the session
-      session.connect(opentokToken, function (error) {
-        // If the connection is successful, publish to the session
-        if (error) {
-          handleError(error);
-        } else {
-          session.publish(publisher, handleError);
-          console.log("Connected to the session.");
-        }
-      });
-    }
-
-    function disconnect() {
-      var session = OT.initSession(opentokApiKey, opentokSessionId);
-      session.disconnect();
-      console.log("Disconnected from the session.");
-    }
+    screenSharingButton.addEventListener("click", (event) => {
+      screenSharing();
+    });
   }
-}
+};
 
 export { initOpentok };
