8449869b820b28c270e29a02dacb88f69f0a3f08
Cataline Baert <snuf_kin@yahoo.com>
2020-12-16 12:46:38 +0100

    complete show method to display all transactions

--- app/controllers/wallets_controller.rb
+++ app/controllers/wallets_controller.rb
@@ -1,6 +1,14 @@
 class WalletsController < ApplicationController
   def show
+    @tutor = Tutor.find(params[:tutor_id])
+    @bookings = @tutor.bookings.where.not(paid_at: nil)
     @wallet = Wallet.find(params[:id])
+    bookings_total = @bookings.map do |booking|
+      booking.booking_price
+    end
+    # change logic for wallet
+    @wallet.amount = bookings_total.sum
+    @wallet.save
   end
 
   def update
